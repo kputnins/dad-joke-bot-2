2a925278ecb20930459300d18e543bb1a42cc28b
Kaspars Putniņš <kaspars.putnins19@gmail.com>
2022-08-21 21:37:12 +0300

    fix: provided window id for puppeteer to run from cron job

--- src/facebook/getDefaultBrowser.ts
+++ src/facebook/getDefaultBrowser.ts
@@ -8,6 +8,9 @@ export const getDefaultBrowser = async (
   const browser = await launch({
     headless,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    env: {
+      DISPLAY: ':1',
+    },
   });
   const context = browser.defaultBrowserContext();
   context.overridePermissions(FACEBOOK_URL, []);
